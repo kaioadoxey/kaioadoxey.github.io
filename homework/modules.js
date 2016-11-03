bd3ec0a3a64137941e2ed46d7da9d7f1f7cdfc45
kaioadoxey <dox15004@byui.edu>
2016-11-02 20:28:12 -0600

    modules_update

---
+++ homework/modules.js
@@ -0,0 +1,9 @@
+$(function() {
+$("#page-header").load("modules/header.html");
+});
+$(function() {
+$("#page-nav").load("modules/nav.html");
+});
+$(function() {
+$("#page-footer").load("modules/footer.html");
+});
