1d8e3e92c43afe6ba3928babf6d79f8ee7ab51e1
kaioadoxey <dox15004@byui.edu>
2016-12-14 18:55:51 -0700

    web_project3

--- web_project/js/modules.js
+++ web_project/js/modules.js
@@ -5,3 +5,7 @@ $("#page-header").load("/web_project/modules/header.html");
 $(function() {
 $("#page-footer").load("/web_project/modules/footer.html");
 });
+
+$(function() {
+$("#page-nav").load("/web_project/modules/nav.html");
+});
